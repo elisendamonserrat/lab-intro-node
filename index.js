6d6dc324ec29492623fb42ba43c1d848e4d4afee
Elisenda <elisenda.monserrat@gmail.com>
2021-04-09 20:12:36 +0200

    does iteration 1 to 5

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined ){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(!this.items.length) {
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if(!this.items.length) {
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    
+  }
 
   avg() {}
 }
